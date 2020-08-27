77c509a214df1d0442e7d2603320b89b13f8b619
robertoleitealves <robertoleite.rl@gmail.com>
2020-08-27 23:40:29 +0000

    Revisão JS 27-08

---
+++ arrow-functions.js
@@ -0,0 +1,38 @@
+//Expressão de função "tradicional"
+
+//Característica dessa função
+// - Um único argumento -> n
+// - Seu corpo tem apenas uma linha de código com return
+
+
+let quadrado = function(n) {
+    return n * n
+}
+
+//A função tradicional acima pode ser transformada em uma ARROW FUNCTION
+// - Desaparece a palavra function
+// - O argumento não precisa de parênteses
+// - Após o argumento há a flecha (arrow) =>
+// - As chaves e a palavra return são omitidos
+
+let quadrado2 = n => n * n
+console.log(quadrado(6))
+console.log(quadrado2(6))
+
+//Situação alternativa 1: função sem argumento
+
+let aleatorio = function(){
+    //Math.random () -> gera um número aleatório entre 0 e 1(fracional) 
+    //Math.floor () -> "corta fora" as casas decimais de um número
+    return Math.floor(Math.random() * 1000) 
+}
+
+
+console.log('---------------')
+console.log(aleatorio())
+
+//arrow function com 0 argumento
+// - os parênteses vazios marcam o lugar do argumento (obrigatórios)
+
+let aleatorio2 = () => Math.random(Math.floor() * 1000)
+
---
+++ comparacoes.js
@@ -0,0 +1,5 @@
+//Comparando duas strings distintas
+let x = 'abacaxi', y = 'jabuticaba'
+console.log(x > y)
+console.log(y > x)
+
