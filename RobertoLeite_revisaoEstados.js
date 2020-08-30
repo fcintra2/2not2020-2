0f90749e092e32311a1bd1b22a66f9ee551bd9cd
robertoleitealves <robertoleite.rl@gmail.com>
2020-08-30 21:37:29 +0000

    Revisão Estados vetores

---
+++ RobertoLeite_revisaoEstados.js
@@ -0,0 +1,20 @@
+// Criação de vetores iniciais
+
+let vetest = ['Alagoas', 'Rio Grande do Norte', 'Paraíba', 'Pernambuco', 'Ceará', 'Piauí', 'Maranhão', 'Bahia']
+let vetsigla = ['AL', 'RN', 'PB', 'PE', 'CE', 'PI', 'MA', 'BA']
+let vetarea = [27848.14, 52811.05, 56469.78, 98148.32, 148920.47, 251577.74, 331937.45, 564733.18]
+let vetpop = [3322820, 3479010, 3996496, 9496294, 9075649, 3264531, 7035055, 14812617]
+let vetest1 = []
+let vetsigla1 = []
+let vetarea1 = []
+let vetpop1 = []
+
+//Inserção de dados no vetor
+//push() -> adiciona novos elementos AO FINAL do vetor
+
+vetest1.push('Sergipe')
+vetsigla1.push('SE')
+vetarea1.push(21915.08)
+vetpop1.push(2278308)
+
+
